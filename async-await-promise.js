b927967fc902b22137434c772b7c021fac759144
alexander bighill <alex@bighill.org>
2022-03-21 21:30:33 -0700

    Arrow functions are cooler.

--- async-await-promise.js
+++ async-await-promise.js
@@ -1,15 +1,16 @@
-const thePromise = (message, delayInMS) => new Promise(resolve => {
-    setTimeout(() => resolve(message), delayInMS)
-})
+const thePromise = (message, delayInMS) =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(message), delayInMS);
+  });
 
-async function doThePromise() {
-    const a = await thePromise('Waiting...', 333)
-    console.log(a)
-    return await thePromise('Done.', 555)
-}
+const doThePromise = async () => {
+  const a = await thePromise("Waiting...", 333);
+  console.log(a);
+  return await thePromise("Done.", 555);
+};
 
-doThePromise().then(res => {
-    console.log(res)
-})
+doThePromise().then((res) => {
+  console.log(res);
+});
 
-console.log('Starting...')
+console.log("Starting...");
